99c64332e9944e6d17074181032b26d089192261
zhangyujian <zhangyujian00@gmail.com>
2014-09-24 10:29:54 +0800

    4001

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ exports.config = {
 	keywords: 'NODECMS,文章管理系统,开源管理系统,NODEJS',
 	favicon: '/public/favicon.ico',
 	//Service
-	port: 4000,
+	port: 4001,
 	session_secret: 'NODECMS',
     datapath: path.join(__dirname, '/public/upload/img')
 };
