49a54accbda7de8d3283bf51661422f092070e53
zhangyujian <zhangyujian00@gmail.com>
2014-10-22 15:54:10 +0800

    test

--- routes/xml.js
+++ routes/xml.js
@@ -15,8 +15,23 @@ exports.handleXML = function(source){
 			+'<FromUserName><![CDATA['+username+']]></FromUserName>'
 			+'<CreateTime>'+CreateTime+'</CreateTime>';
 	if(MsgType == "text"){
-		var Content = select(doc, "//Content")[0].firstChild.data;
-		xml = xml + '<MsgType><![CDATA[text]]></MsgType><Content><![CDATA[文本：'+Content+']]></Content>'
+		xml = xml + '<MsgType><![CDATA[news]]></MsgType><ArticleCount>2</ArticleCount>'
+		+'<Articles>'
+		+'<item>'
+		+'<Title><![CDATA[第一篇测试文章]]></Title> '
+		+'<Description><![CDATA[第一篇测试文章的描述]]></Description>'
+		+'<PicUrl><![CDATA[http://upload.chinaz.com/2014/1016/1413428212775.jpg]]></PicUrl>'
+		+'<Url><![CDATA[http://www.baidu.com]]></Url>'
+		+'</item>'
+		+'<item>'
+		+'<Title><![CDATA[第二篇测试文章]]></Title>'
+		+'<Description><![CDATA[第二篇测试文章的描述]]></Description>'
+		+'<PicUrl><![CDATA[http://upload.chinaz.com/2014/1021/1413880631473.jpg]]></PicUrl>'
+		+'<Url><![CDATA[http://www.xiangshang360.com]]></Url>'
+		+'</item>'
+		+'</Articles>'
+		//var Content = select(doc, "//Content")[0].firstChild.data;
+		//xml = xml + '<MsgType><![CDATA[text]]></MsgType><Content><![CDATA[文本：'+Content+']]></Content>'
 	}
 	else{
 		xml = xml + '<MsgType><![CDATA[text]]></MsgType><Content><![CDATA[您发送的是一段非文本信息哦]]></Content>'
